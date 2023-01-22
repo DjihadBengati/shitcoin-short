69e8af0e36b0a8591a31fbb979699bae46883a69
Djihad BENGATI <d.bengati@gmail.com>
2023-01-22 13:24:47 +0100

    :tada:

---
+++ src/main/java/com/db/bot/shitcoinshort/ShitcoinShortApplication.java
@@ -0,0 +1,13 @@
+package com.db.bot.shitcoinshort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitcoinShortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShitcoinShortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/bot/shitcoinshort/ShitcoinShortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.bot.shitcoinshort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShitcoinShortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
